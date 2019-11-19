4a8a7b20c4a67fad2e307456d7cc4febb89d885f
sharu725 <sharu725@gmail.com>
2019-11-19 16:40:46 +0530

    add program section

---
+++ assets/js/fontawesome.js
@@ -0,0 +1,8 @@
+(function() {
+  var css = document.createElement("link");
+  css.href =
+    "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.css";
+  css.rel = "stylesheet";
+  css.type = "text/css";
+  document.getElementsByTagName("head")[0].appendChild(css);
+})();
